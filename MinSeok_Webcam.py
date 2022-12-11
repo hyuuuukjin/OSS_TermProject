9abd8536b337a39592a647d7a38d1557a3933445
Lemon7z <lemon7z@icloud.com>
2022-12-11 21:52:31 +0900

    2022-12-11 Function added

--- MinSeok_Webcam.py
+++ MinSeok_Webcam.py
@@ -3,24 +3,27 @@
 import cv2
 import time
 
-webcam = cv2.VideoCapture(0)
+def webcam_main():
+    webcam = cv2.VideoCapture(0)
 
-if not webcam.isOpened():
-    print("Could not open webcam")
-    exit()
+    if not webcam.isOpened():
+        print("Could not open webcam")
+        exit()
 
-prev_time = 0
-FPS = 10
+    prev_time = 0
+    FPS = 10
 
-while 1:
-    ret, frame = webcam.read()
-    current_time = time.time() - prev_time
-    if (ret is True) and (current_time > 1. / FPS):
-        prev_time = time.time()
-        frame = cv2.flip(frame, 1)
-        cv2.imshow('VideoCapture', frame)
-        if cv2.waitKey(1) > 0:
-            break
+    while 1:
+        ret, frame = webcam.read()
+        current_time = time.time() - prev_time
+        if (ret is True) and (current_time > 1. / FPS):
+            prev_time = time.time()
+            frame = cv2.flip(frame, 1)
+            # cv2.imshow('VideoCapture', frame)
+            if cv2.waitKey(1) > 0:
+                break
 
-webcam.release()
-cv2.destroyAllWindows()
+    webcam.release()
+    cv2.destroyAllWindows()
+
+webcam_main()
