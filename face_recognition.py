4c36a31b20538c1dbf2067f40307014cce0c4b63
hyuuuukjin <106862808+hyuuuukjin@users.noreply.github.com>
2022-12-12 18:25:30 +0900

    Update face_recognition.py
    Update return, var_name

--- face_recognition.py
+++ face_recognition.py
@@ -1,14 +1,15 @@
 import cv2
-
-def face_recognition(cam):
+face_recognition = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def face_recog(cam):
+    gray = cv2.cvtColor(cam, cv2.COLOR_BGR2GRAY)
     faces = face_recognition.detectMultiScale(gray, 1.05, 4, minSize=(200, 200))
 
     for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.rectangle(cam, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-            cv2.imshow('Video', frame)
+            # cv2.imshow('Video', frame)
 
-    if faces >= 1:
-        return 1
+    if len(faces) >= 1:
+        return 1, cam, faces
     else:
-        return 0
+        return 0, cam, faces
