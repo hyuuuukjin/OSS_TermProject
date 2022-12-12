f76e09fbc0cf6d7bcce50a52150665a7ea06d926
hyuuuukjin <106862808+hyuuuukjin@users.noreply.github.com>
2022-12-13 00:29:31 +0900

    lim_determine.py fixed

--- lim_determine.py
+++ lim_determine.py
@@ -2,6 +2,6 @@ import cv2
 import time
 import face_recognition
 
-def check_face(frame):
-    val = face_recognition.face_recog(frame)
-    return val[0]
+def check_face(frame, output):
+    val = face_recognition.face_recog(frame, output)
+    return val[0], val[1]
