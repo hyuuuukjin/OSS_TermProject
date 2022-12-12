959cabd172d7589b236d9620e9ba8265d5480ab3
Lemon7z <lemon7z@icloud.com>
2022-12-12 18:23:28 +0900

    2022-12-11 main and webcam function fixed.

--- MinSeok_Webcam.py
+++ MinSeok_Webcam.py
@@ -1,29 +1,37 @@
 #202235134 MinSeok Choi
-
 import cv2
 import time
+import determine_person
+# import FaceMesh
 
 def webcam_main():
+    check = 0
     webcam = cv2.VideoCapture(0)
-
     if not webcam.isOpened():
         print("Could not open webcam")
         exit()
-
     prev_time = 0
-    FPS = 10
-
+    FPS = 2
+    curframe = 0
     while 1:
         ret, frame = webcam.read()
         current_time = time.time() - prev_time
         if (ret is True) and (current_time > 1. / FPS):
+            curframe += 1
             prev_time = time.time()
             frame = cv2.flip(frame, 1)
-            # cv2.imshow('VideoCapture', frame)
+            # frame1 = FaceMesh.Pritreatment(frame)
+            check += determine_person.check_face(frame)
+            # cv2.imshow("test", frame)
             if cv2.waitKey(1) > 0:
                 break
+        if curframe == 10:
+            if check >= 7:
+                print("face detected")
+            else:
+                print("No face")
+            curframe = 0
+            check = 0
 
     webcam.release()
     cv2.destroyAllWindows()
-
-webcam_main()
---
+++ main.py
@@ -0,0 +1,5 @@
+
+import MinSeok_Webcam
+
+if __name__ == "__main__":
+    MinSeok_Webcam.webcam_main()
